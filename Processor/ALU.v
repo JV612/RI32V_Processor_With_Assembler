c15b25b80c992c03660168829729723f67b63f8f
Jay Vaghasiya <149051383+JV612@users.noreply.github.com>
2025-06-07 20:59:05 +0530

    Updated ALU.v

--- Processor/ALU.v
+++ Processor/ALU.v
@@ -1,5 +1,4 @@
 module ALU (
-
     input wire [31:0] A,               // First operand
     input wire [31:0] B,               // Second operand
     input wire isAdd,                  // Control signal for addition
@@ -15,11 +14,9 @@ module ALU (
     input wire isLsl,                  // Control signal for left shift logical
     input wire isLsr,                  // Control signal for right shift logical
     input wire isAsr,                  // Current flags register passed from ExecuteUnit
-    
     output reg [31:0] aluResult,       // Result of ALU operation
     output reg flag_eq,
-    output reg flag_gt
-
+    output reg flag_gt         
 );
 
 wire [3:0] local_flags;                 // Local flags register
